7eaa0f8c8aec6f2dd42b2de6e4a763930209edcb
Solomon Lucas-Thornton <solomonlucasthornton@gmail.com>
2020-09-24 01:54:38 +0000

    fixed main3

--- main3.c
+++ main3.c
@@ -3,41 +3,35 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-void ChildProcess(void);
-void ParentProcess(void);
-
 void main(void){
-  pid_t  pid;
-  pid = fork();
-  srand(time(NULL));
-  if (pid == 0){
-    ChildProcess();
-  }
-  else{
-    ParentProcess();
-  }
-  
-  
-}
-
-void  ChildProcess(void)
-{
-  srand(time(NULL));
-  int pid = getpid();
-  int ppid = getppid();
-  int range_loop = (rand() % 30) + 1;
-  int range_sleep = (rand() % 10) + 1;
+  int pid;
   int i;
-  for(i = 0; i < range_loop; i++){
-     printf("Child PID: %d is going to sleep!\n", pid);
-     sleep(range_sleep);
-     printf("Child %d: I'm awake!\n Where is my Parent %d?\n", pid, ppid);
-   }
- 
-  exit(0);
-}
+  srand(time(NULL));
+  
+  for(i =0; i < 2;++i){
+  pid = fork();
+  
+    if (pid == 0){
+      int range_loop = (rand() % 30) + 1;
+      int range_sleep = (rand() % 10) + 1;
+      int i;
+      for(i = 0; i < range_loop; i++){
+         printf("Child PID: %d is going to sleep!\n", getpid());
+         sleep(range_sleep);
+         printf("Child %d: I'm awake!\n Where is my Parent %d?\n", getpid(), getppid());
+       }
+      exit(0);
 
-void  ParentProcess(void)
-{
-  printf("Child Pid: %d has completed!\n", wait());
+    }
+    else if(pid >0){
+      int status = wait(&status);
+
+      printf("Child Pid: %d has completed!\n",status);
+    }
+    else{
+      printf("Woops, something went wrong!");
+
+      exit(0);
+    }
+  }
 }
